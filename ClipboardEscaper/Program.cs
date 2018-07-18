2d99706235e1ce5c90ab2e2c99c0965106915662
Bo Bendtsen <bben@dr.dk>
2018-07-18 15:55:49 +0200

    Add project files.

---
+++ ClipboardEscaper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClipboardEscaper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
